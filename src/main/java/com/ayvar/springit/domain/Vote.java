93b93692c20525bd2af4d31ac48aad19223f96e0
Daniel Ayvar <dayvar141@gmail.com>
2021-03-29 16:32:34 -0500

    Updated application properties with basic h2 & datasource setup

---
+++ src/main/java/com/ayvar/springit/domain/Vote.java
@@ -0,0 +1,21 @@
+package com.ayvar.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+
+}
---
+++ src/main/java/com/ayvar/springit/repository/LinkRepository.java
@@ -0,0 +1,8 @@
+package com.ayvar.springit.repository;
+
+import com.ayvar.springit.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link,Long> {
+    Link findByTitle(String title);
+}
