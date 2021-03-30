ebd7905fe8fcd6096a9f2710d7c41cb7d7c0b3f9
Daniel Ayvar <dayvar141@gmail.com>
2021-03-30 17:15:11 -0500

    Setting up our memory database

--- src/main/java/com/ayvar/springit/SpringitApplication.java
+++ src/main/java/com/ayvar/springit/SpringitApplication.java
@@ -1,9 +1,17 @@
 package com.ayvar.springit;
 
+import com.ayvar.springit.domain.Comment;
+import com.ayvar.springit.domain.Link;
+import com.ayvar.springit.repository.CommentRepository;
+import com.ayvar.springit.repository.LinkRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringitApplication {
 
     public static void main(String[] args) {
@@ -11,4 +19,18 @@ public class SpringitApplication {
         System.out.println("Welcome to Springit!");
     }
 
+    @Bean
+    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
+        return args -> {
+          Link link = new Link("Getting Started with Spring Boot 2", "https://therealdanvega.com/spring-boot-2");
+          linkRepository.save(link);
+          Comment comment = new Comment("This course is awesome", link);
+          commentRepository.save(comment);
+          link.addComment(comment);
+
+          Link firstLink = linkRepository.findByTitle("Getting Started with Spring Boot 2");
+          System.out.println(firstLink.getTitle());
+
+        };
+    }
 }
--- src/main/java/com/ayvar/springit/domain/Link.java
+++ src/main/java/com/ayvar/springit/domain/Link.java
@@ -2,6 +2,8 @@ package com.ayvar.springit.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,17 +14,24 @@ import java.util.List;
 
 @Entity
 @Data
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Link extends Auditable{
 
     @Id
     @GeneratedValue
     private long id;
+    @NonNull
     private String title;
+    @NonNull
     private String url;
 
 
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<Comment>();
+
+    public void addComment(Comment comment){
+        this.comments.add(comment);
+    }
 
 }
